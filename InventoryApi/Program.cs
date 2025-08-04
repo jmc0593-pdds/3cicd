755763e670eec3353d2c46b982adfe9007f3302c
Jorge <jorgemario.cardonaoliva@gmail.com>
2025-08-03 20:31:04 -0600

    Fixing pipeline and reorder program.cs

--- InventoryApi/Program.cs
+++ InventoryApi/Program.cs
@@ -1,9 +1,15 @@
+using System.Reflection.Metadata.Ecma335;
 using InventoryApi.Data;
 using InventoryApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connString = builder.Configuration.GetConnectionString("Inventory");
+
 builder.Services.AddSqlite<InventoryContext>(connString);
+
 var app = builder.Build();
+
 app.MapProductEndpoints();
+
 app.Run();
